1530a2f8c5ca3319083c583721d0294fb0bf2a55
VillarDeveloper <villar.developer@gmail.com>
2022-05-25 11:59:44 -0700

    tester

--- CRUD MVVM/CRUD MVVM/Model/Client.swift
+++ CRUD MVVM/CRUD MVVM/Model/Client.swift
@@ -20,6 +20,7 @@ struct Client {
     var account: Int
 }
 //teste alteração git2
+// comentário git 
 
 //var clients: [Client] = [Client(name: "", cpf: "", birthDate: "", email: "", monthlyIncome: "", netWorth: "", password: "", balance: 99.9)]
 var clients: [Client] = []
