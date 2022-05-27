69ecddc4420779ba00bac3d48bda59c4e2ad2110
@luciano.savi <luciano.savi@foursys.com.br>
2022-05-27 06:03:40 -0700

    Luciano- Criação das viewModels e começo da tela de confirmação do cobrar

---
+++ CRUD MVVM/CRUD MVVM/Scene/PixCobrar/cobrarViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  cobrarViewModel.swift
+//  CRUD MVVM
+//
+//  Created by user220270 on 5/27/22.
+//
+
+import Foundation
---
+++ CRUD MVVM/CRUD MVVM/Scene/PixCobrar/confirmViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  confirmViewModel.swift
+//  CRUD MVVM
+//
+//  Created by user220270 on 5/27/22.
+//
+
+import Foundation
